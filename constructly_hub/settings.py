3ff67062b1b13c66bdf0d12d3bba5dda16b09bfc
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2025-10-12 20:08:10 +0000

    Securing Django app

--- constructly_hub/settings.py
+++ constructly_hub/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-mm7hg92oh6^(w^tl9@#e1i)oqjjslt+z1mehi@0$f_imgzz-2e"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
