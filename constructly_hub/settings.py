f9f7ccde927988b521e2da88058f14795f46cf18
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2025-10-30 23:17:05 +0000

    Style password reset for future implementation

--- constructly_hub/settings.py
+++ constructly_hub/settings.py
@@ -119,6 +119,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
