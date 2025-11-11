ad9740b3b1e3afe03cec5589d38e9007f25a5352
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2025-11-11 20:44:10 +0000

    Create profile table

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'full_name', 'display_email', 'phone', 'updated_on')
+    search_fields = ('user', 'full_name', 'display_email', 'phone')
+    list_filter = ('updated_on',)
+    ordering = ('-updated_on',)
--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    avatar = CloudinaryField('image', default='default_avatar')
+    display_email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    bio = models.TextField(max_length=1000, blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-updated_on"]
+        verbose_name_plural = "User Profiles"
